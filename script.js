d40b8a8a6b0a3eaedb2fb7cbfed102e7148c9bfe
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-07-31 11:23:02 +0700

    adding parallax effect on scrolling event

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const textJbtron = document.querySelector('.text-jbtron');
 const linkJbtron = document.querySelector('.link-jbtron');
 const imgJbtron = document.querySelector('.jbtron-img');
+const bodyContainer = document.querySelector('.body-container');
 
 window.addEventListener('load', () => {
     textJbtron.style.animation = 'animasiLoadTextDown 3s';
     linkJbtron.style.animation = 'animasiLoadTextUp 3s';
     imgJbtron.style.animation = 'animasiLoadImg 3s';
+    bodyContainer.style.opacity = 0;
 });
 
-document.addEventListener('scroll', function (e) {
+document.addEventListener('scroll', () => {
     let position = Math.round(window.scrollY);
-    console.log(position);
+    let imgPosition = Math.round(imgJbtron.getBoundingClientRect().top);
+    let slowImgPosition = ((Math.round(imgJbtron.getBoundingClientRect().top)) / 1.5) * -1;
+
+    imgJbtron.style.transform = `translateY(${slowImgPosition}px)`;
+    imgJbtron.style.zIndex = `-999`;
+
+    if (position > 50) {
+        bodyContainer.style.animation = 'animasiLoadImg 2s forwards';
+    }
 });
+
